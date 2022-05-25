b2e257d8c9734092b04deb906243ca8e540242f4
ChristopherDocherty <christopher.docherty@googlemail.com>
2022-05-25 18:06:00 +0100

    Rewrote condor related files, updated .gitignore

---
+++ condor_script.sh
@@ -0,0 +1,6 @@
+#!/bin/bash 
+
+#Provide the location of your pip activate  
+source /home/your_env/bin/activate
+
+python $@
--- src/utils/EE.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash 
-
-source /home/2312403d/masters/schwarzchild/PINN/bin/activate
-
-
-time python $@
